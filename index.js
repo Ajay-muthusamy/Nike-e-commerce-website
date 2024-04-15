519ac86a820c9026ca54fbc6a29b9cb923eaaddf
Ajay-muthusamy <ajaym.22cse@kongu.edu>
2024-04-15 19:45:49 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const navbar = document.querySelector('.nav-bar');
+navbar.style.maxHeight = "0px";
+function togglemenu(){
+    if(navbar.style.maxHeight == "0px"){
+        navbar.style.maxHeight = "300px"
+    }
+    else{
+        navbar.style.maxHeight = "0px"
+    }
+}
